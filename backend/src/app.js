68478644fd4660bddbf7d49693c3638ccddfdcb3
maxgogol228 <maxgogolonline@gmail.com>
2026-04-25 20:44:14 +0300

    Delete backend/src directory

--- backend/src/app.js
+++
@@ -1,5 +0,0 @@
-app.use(require("./middleware/userLogger"));
-app.use("/api/devices", deviceRoutes);
-app.use("/api/items", itemRoutes);
-app.use("/api/system", systemRoutes);
-app.use("/api/logs", logRoutes);
