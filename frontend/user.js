9658fce5009ff3f53fb0f60e65828e46cbef9c88
maxgogol228 <maxgogolonline@gmail.com>
2026-04-25 21:15:38 +0300

    Delete frontend directory

--- frontend/user.js
+++
@@ -1,9 +0,0 @@
-
-export function getUser(){
-  let user = localStorage.getItem("username");
-  if(!user){
-    user = prompt("Введите ваше имя");
-    localStorage.setItem("username", user);
-  }
-  return user;
-}
