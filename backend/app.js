ebaf7f246672bac36070214f464e060cec03e322
maxgogol228 <maxgogolonline@gmail.com>
2026-04-27 19:46:10 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const systemRoutes = require("./routes/systemRoutes");
 const userLogger = require("./middleware/userLogger");
+const { pool } = require("./db/db");
+
+app.get("/api/test-db", async (req, res) => {
+  const result = await pool.query("SELECT NOW()");
+  res.json(result.rows);
+});
 
 const app = express();
 app.use(cors());
