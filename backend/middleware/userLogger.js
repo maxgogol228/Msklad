b11051d575a555a071c968d4aa1ea703ebeee243
maxgogol228 <maxgogolonline@gmail.com>
2026-04-27 21:11:07 +0300

    Delete backend directory

--- backend/middleware/userLogger.js
+++
@@ -1,5 +0,0 @@
-module.exports = (req,res,next)=>{
-  const user = req.headers["x-user"] || "unknown";
-  console.log(user + " -> " + req.method + " " + req.url);
-  next();
-};
--- backend/routes/systemRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const { pool } = require("../db/db");
-
-router.get("/items", async (req, res) => {
-  const result = await pool.query("SELECT * FROM items");
-  res.json(result.rows);
-});
-
-module.exports = router;
