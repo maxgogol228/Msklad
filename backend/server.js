baafb70621895d38ee05ed53a7b8e23f9e8793f2
maxgogol228 <maxgogolonline@gmail.com>
2026-04-27 19:43:10 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,12 @@
 const app = require("./app");
+const { initDB } = require("./db/db");
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server running on " + PORT));
+
+// запускаем БД
+initDB();
+
+// запускаем сервер
+app.listen(PORT, () => {
+  console.log("🚀 Server running on port " + PORT);
+});
