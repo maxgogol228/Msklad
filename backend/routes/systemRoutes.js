0f89b314aa86ef962fec38b09cda9dd8bedb5d6f
maxgogol228 <maxgogolonline@gmail.com>
2026-04-27 19:45:11 +0300

    Update systemRoutes.js

--- backend/routes/systemRoutes.js
+++ backend/routes/systemRoutes.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
+const { pool } = require("../db/db");
 
-router.get("/ping", (req,res)=>{
-  res.json({message:"API работает"});
+router.get("/items", async (req, res) => {
+  const result = await pool.query("SELECT * FROM items");
+  res.json(result.rows);
 });
 
 module.exports = router;
