a0b6aebca5625283225b596ec24870584180e2f6
maxgogol228 <maxgogolonline@gmail.com>
2026-04-29 17:08:31 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import {useEffect,useState} from "react";
 import axios from "axios";
+import React from "react";
 
 const API=axios.create({baseURL:import.meta.env.VITE_API_URL});
 let count=0; setInterval(()=>count=0,60000);
