666a5c5f74e7f3fb0023cb934fd9bacfd9f9a327
maxgogol228 <maxgogolonline@gmail.com>
2026-04-29 19:18:06 +0300

    Add files via upload

---
+++ frontend/src/layout/Sidebar.jsx
@@ -0,0 +1,17 @@
+import { Box, List, ListItemButton } from "@mui/material";
+
+export default function Sidebar({ setPage }) {
+  return (
+    <Box width={220} bgcolor="#111" height="100vh">
+      <List>
+        <ListItemButton onClick={() => setPage("items")}>
+          Детали
+        </ListItemButton>
+
+        <ListItemButton onClick={() => setPage("devices")}>
+          Приборы
+        </ListItemButton>
+      </List>
+    </Box>
+  );
+}
