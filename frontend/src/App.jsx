fb304ca72c577c5b0748f6e030e271129d99ea92
maxgogol228 <maxgogolonline@gmail.com>
2026-04-29 19:14:17 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { ThemeProvider } from "@mui/material";
+import { ThemeProvider, CssBaseline } from "@mui/material";
 import theme from "./theme";
+
+// главный layout (sidebar + topbar + страницы)
 import Layout from "./layout/Layout";
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
+      {/* сброс стандартных браузерных стилей */}
+      <CssBaseline />
+
+      {/* основной интерфейс */}
       <Layout />
     </ThemeProvider>
   );
