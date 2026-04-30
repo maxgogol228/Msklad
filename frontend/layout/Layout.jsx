ea8a81179390ae83c679abfdd9bd72a14d74a713
maxgogol228 <maxgogolonline@gmail.com>
2026-04-30 21:58:34 +0300

    Update Layout.jsx

--- frontend/layout/Layout.jsx
+++ frontend/layout/Layout.jsx
@@ -1,28 +1,37 @@
-import React, { useState } from "react";
 import { Box } from "@mui/material";
+import { useState } from "react";
 import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
+
 import ItemsPage from "../pages/ItemsPage";
+import ConsumablesPage from "../pages/ConsumablesPage";
 import DevicesPage from "../pages/DevicesPage";
+
 import StateButtons from "../components/StateButtons";
+import AntiDDOS from "../components/AntiDDOS";
 
 export default function Layout() {
   const [page, setPage] = useState("items");
 
   return (
-    <Box display="flex">
-      <Sidebar setPage={setPage} />
+    <>
+      <AntiDDOS />
 
-      <Box flex={1}>
-        <Topbar />
+      <Box display="flex">
+        <Sidebar setPage={setPage} />
 
-        <Box p={2}>
-          {page === "items" && <ItemsPage />}
-          {page === "devices" && <DevicesPage />}
+        <Box flex={1}>
+          <Topbar />
+
+          <Box p={2}>
+            {page === "items" && <ItemsPage />}
+            {page === "consumables" && <ConsumablesPage />}
+            {page === "devices" && <DevicesPage />}
+          </Box>
         </Box>
       </Box>
 
       <StateButtons />
-    </Box>
+    </>
   );
 }
