2d2813d47c1105e126a9decf53957225284d7e16
maxgogol228 <maxgogolonline@gmail.com>
2026-04-30 21:59:10 +0300

    Update Topbar.jsx

--- frontend/layout/Topbar.jsx
+++ frontend/layout/Topbar.jsx
@@ -2,7 +2,7 @@ import { Box, Typography } from "@mui/material";
 
 export default function Topbar() {
   return (
-    <Box height={60} display="flex" alignItems="center" px={2}>
+    <Box height={60} display="flex" alignItems="center" px={2} borderBottom="1px solid #333">
       <Typography variant="h6">М склад</Typography>
     </Box>
   );
