14fca07125284f5e6c65420bc9f77cd509293d18
maxgogol228 <maxgogolonline@gmail.com>
2026-04-30 21:58:55 +0300

    Update Sidebar.jsx

--- frontend/layout/Sidebar.jsx
+++ frontend/layout/Sidebar.jsx
@@ -4,13 +4,9 @@ export default function Sidebar({ setPage }) {
   return (
     <Box width={220} bgcolor="#111" height="100vh">
       <List>
-        <ListItemButton onClick={() => setPage("items")}>
-          Детали
-        </ListItemButton>
-
-        <ListItemButton onClick={() => setPage("devices")}>
-          Приборы
-        </ListItemButton>
+        <ListItemButton onClick={() => setPage("items")}>Детали</ListItemButton>
+        <ListItemButton onClick={() => setPage("consumables")}>Расходники</ListItemButton>
+        <ListItemButton onClick={() => setPage("devices")}>Приборы</ListItemButton>
       </List>
     </Box>
   );
