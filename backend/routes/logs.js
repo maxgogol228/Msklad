44c57ce9d7ebe97e42c6fadba13d78fa061229f8
maxgogol228 <maxgogolonline@gmail.com>
2026-05-04 20:44:09 +0300

    Update logs.js

--- backend/routes/logs.js
+++ backend/routes/logs.js
@@ -2,19 +2,8 @@ const router = require("express").Router();
 const db = require("../db");
 
 router.get("/", async (req, res) => {
-  const result = await db.query("SELECT * FROM logs ORDER BY id DESC");
-  res.json(result.rows);
-});
-
-router.post("/", async (req, res) => {
-  const { action, user } = req.body;
-
-  await db.query(
-    "INSERT INTO logs(action, user_name) VALUES($1,$2)",
-    [action, user]
-  );
-
-  res.sendStatus(200);
+  const r = await db.query("SELECT * FROM logs ORDER BY id DESC");
+  res.json(r.rows);
 });
 
 module.exports = router;
