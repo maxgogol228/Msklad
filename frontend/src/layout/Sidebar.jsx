f1a72e076ae0b7641664e9b45297f08d6e62c019
maxgogol228 <maxgogolonline@gmail.com>
2026-05-07 17:26:54 +0300

    Update Sidebar.jsx

--- frontend/src/layout/Sidebar.jsx
+++ frontend/src/layout/Sidebar.jsx
@@ -1,13 +1,43 @@
-import { Box, List, ListItemButton } from "@mui/material";
-
 export default function Sidebar({ setPage }) {
   return (
-    <Box width={220} bgcolor="#111" height="100vh">
-      <List>
-        <ListItemButton onClick={() => setPage("items")}>Детали</ListItemButton>
-        <ListItemButton onClick={() => setPage("consumables")}>Расходники</ListItemButton>
-        <ListItemButton onClick={() => setPage("devices")}>Приборы</ListItemButton>
-      </List>
-    </Box>
+    <div style={{
+      width: 220,
+      background: "#111",
+      height: "100vh",
+      color: "#fff"
+    }}>
+      <div style={{ padding: 10 }}>
+        <button 
+          onClick={() => setPage("items")}
+          style={buttonStyle}
+        >
+          Детали
+        </button>
+        <button 
+          onClick={() => setPage("consumables")}
+          style={buttonStyle}
+        >
+          Расходники
+        </button>
+        <button 
+          onClick={() => setPage("devices")}
+          style={buttonStyle}
+        >
+          Приборы
+        </button>
+      </div>
+    </div>
   );
 }
+
+const buttonStyle = {
+  display: "block",
+  width: "100%",
+  padding: "10px",
+  marginBottom: "5px",
+  background: "#333",
+  color: "#fff",
+  border: "none",
+  cursor: "pointer",
+  textAlign: "left"
+};
