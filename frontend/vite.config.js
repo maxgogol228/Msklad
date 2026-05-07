c4a94515682f6e86fd785917fc50ee13c83b1083
maxgogol228 <maxgogolonline@gmail.com>
2026-05-07 21:53:43 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/", // ВАЖНО
+  plugins: [
+    react({
+      jsxRuntime: 'automatic', // Это должно быть по умолчанию
+    })
+  ],
+  base: "/",
+  esbuild: {
+    jsxInject: `import React from 'react'`, // Добавляет импорт React автоматически
+  }
 });
