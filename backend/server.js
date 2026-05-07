064d0b6bde112ccbc7a81631c4e95ed149fdc8ad
maxgogol228 <maxgogolonline@gmail.com>
2026-05-07 21:06:55 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const initDb = require("./initDb");
-const authRoutes = require("./routes/auth");
 
 const app = express();
 
@@ -15,12 +14,10 @@ app.use("/items", require("./routes/items"));
 app.use("/consumables", require("./routes/consumables"));
 app.use("/devices", require("./routes/devices"));
 app.use("/logs", require("./routes/logs"));
+app.use("/categories", require("./routes/categories")); // НОВЫЙ МАРШРУТ
 
 app.get("/", (req, res) => {
   res.send("API работает");
-});
-app.get("/system/ping", (req, res) => {
-  res.json({ status: "ok" });
 });
 
 const PORT = process.env.PORT || 10000;
