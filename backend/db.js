8f6f4137de49d5a4c30fba715d8e7dca70ddb417
maxgogol228 <maxgogolonline@gmail.com>
2026-06-04 17:40:21 +0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,10 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
+  statement_timeout: 300000, // 5 минут на запрос
+  query_timeout: 300000,
+  connectionTimeoutMillis: 30000
 });
 
 module.exports = {
