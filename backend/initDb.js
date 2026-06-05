d5e7598040e6b1204e5ba7526e954429f374a19d
maxgogol228 <maxgogolonline@gmail.com>
2026-06-05 15:58:32 +0300

    Update initDb.js

--- backend/initDb.js
+++ backend/initDb.js
@@ -24,6 +24,7 @@ module.exports = async function initDb() {
         login TEXT UNIQUE,
         access_key TEXT,
         approved BOOLEAN DEFAULT false,
+        name TEXT UNIQUE,
         is_admin BOOLEAN DEFAULT false,
         created_at TIMESTAMP DEFAULT NOW()
       );
